43b25a97aff78fcbf357d6c628d3232ac677d6bb
Nicholas <nbm@localhost.localdomain>
2014-06-18 23:55:07 -0400

    have arithmetic and list functions

--- Scheme.lisp
+++ Scheme.lisp
@@ -6,38 +6,21 @@
 (defun revalp ()
 	(set-functions)
 	(format t "~% -->")
-	(print (evall (read)))
+	(print (eval-2 (read)))
 	(revalp))
-
-(defun evall (expr)
-	(if (consp expr)
-		(eval-compound expr)
-		(if (atomp expr)
-			expr)))
 
 (defun atomp (x)
 	(if (numberp x) t nil))
 
-;;; evaluates a scheme expression
-;;; need to change the order of evaluation
-;;; need to evaluate the arguments, then the function itself
-(defun eval-compound (expr)
-	(let ((op (car expr))
-				(og (car expr))
-				(numbers (rest expr))
-				(flag nil))
-		(multiple-value-setq  (op flag) (get-function og))
-		(if (eql 1 (length numbers))
-			(car numbers)
-			(when (eql flag t)  
-				(if (consp (car numbers))
-					(setf (car numbers) (eval-compound (car numbers)))) ; checks to make sure first arg is not a list itself
-				(if (consp (cadr numbers))
-					(setf (cadr numbers) (eval-compound (cadr numbers)))) ; checks second arg 
-				(setf (car numbers) (apply op (list (car numbers) (cadr numbers)))) 
-				(if (< 1 (length numbers))
-					(setf numbers (remove (cadr numbers) numbers)))
-				(eval-compound `(,og ,@numbers)))))) 
+(defun eval-2 (expr)
+	(if (atomp expr)
+		expr
+		(progn
+			(multiple-value-setq (op flag) (get-function (car expr)))
+			(if (eql flag t)
+				(apply op (mapcar #'eval-2 (rest expr)))
+				(car expr)))))
+
 
 (defvar *func-table* (make-hash-table :test #'equal))
 
@@ -49,7 +32,8 @@
 	(setf (gethash '= *func-table*) #'=)
 	(setf (gethash 'list *func-table*) #'list)
 	(setf (gethash 'car *func-table*) #'car)
-	(setf (gethash 'cdr *func-table*) #'cdr))
+	(setf (gethash 'cdr *func-table*) #'cdr)
+	(setf (gethash 'cons *func-table*) #'cons))
 
 (defun get-function (op)
 	(gethash op *func-table*))
